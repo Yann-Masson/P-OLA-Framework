109333a1de60f8c8ef494bbc78d920fd54ff7939
Antoine Grand <114659327+agrand2004@users.noreply.github.com>
2026-02-13 17:04:03 +0100

    Test of DIC library (#1)
    * feat(DI): add some test interfaces with simple and mocked implementations and a main file to test the difference between simple and DI implementation of those interfaces
    
    * feat(DI): add the DI circular injection of the provider
    
    * delete(architecture): remove all of the testing files for the DI test
    
    * fix(linter): add a new line at the end of main.cpp

--- CppDic/lib/include/cppdic/ServiceProvider.hpp
+++ CppDic/lib/include/cppdic/ServiceProvider.hpp
@@ -3,24 +3,74 @@
 #include <cppdic/Service.hpp>
 #include <cppdic/Utils.hpp>
 
-namespace dic
-{
+#include <memory>
+#include <tuple>
+#include <typeindex>
 
-    template<class... Ts>
-    class [[nodiscard]] ServiceProvider final
-    {
-    public:
-        explicit ServiceProvider(Services<Ts...> _impls) : impls(_impls) {}
+namespace dic {
 
-    public:
-        template<class T>
-        std::shared_ptr<T> get()
-        {
-            constexpr auto index = utils::getIndex<T, Ts...>(0);
-            return std::get<index>(impls);
-        }
+class ServiceProviderRef {
+public:
+  ServiceProviderRef() = default;
 
-    private:
-        Services<Ts...> impls;
-    };
+  template <class T> std::shared_ptr<T> get() const {
+    if (getRawFn == nullptr) {
+      return nullptr;
+    }
+
+    auto raw = getRawFn(impls, std::type_index(typeid(T)));
+    return std::static_pointer_cast<T>(raw);
+  }
+
+private:
+  using GetRawFn = std::shared_ptr<void> (*)(const std::shared_ptr<void> &,
+                                             std::type_index);
+
+  ServiceProviderRef(std::shared_ptr<void> _impls, GetRawFn _getRawFn)
+      : impls(std::move(_impls)), getRawFn(_getRawFn) {}
+
+  std::shared_ptr<void> impls;
+  GetRawFn getRawFn = nullptr;
+
+  template <class... Ts> friend class ServiceProvider;
+};
+
+template <class... Ts> class [[nodiscard]] ServiceProvider final {
+public:
+  explicit ServiceProvider(std::shared_ptr<Services<Ts...>> _impls)
+      : impls(std::move(_impls)) {}
+
+  ServiceProviderRef ref() const {
+    return ServiceProviderRef(impls, &ServiceProvider::getRaw);
+  }
+
+public:
+  template <class T> std::shared_ptr<T> get() const {
+    constexpr auto index = utils::getIndex<T, Ts...>(0);
+    return std::get<index>(*impls);
+  }
+
+private:
+  static std::shared_ptr<void> getRaw(const std::shared_ptr<void> &impls,
+                                      std::type_index type) {
+    auto typedImpls = std::static_pointer_cast<Services<Ts...>>(impls);
+    return getRawImpl<0, Ts...>(*typedImpls, type);
+  }
+
+  template <size_t Index, class Head, class... Tail>
+  static std::shared_ptr<void> getRawImpl(const Services<Ts...> &impls,
+                                          std::type_index type) {
+    if (type == std::type_index(typeid(typename Head::BaseType))) {
+      return std::static_pointer_cast<void>(std::get<Index>(impls));
+    }
+
+    if constexpr (sizeof...(Tail) > 0) {
+      return getRawImpl<Index + 1, Tail...>(impls, type);
+    }
+
+    return nullptr;
+  }
+
+  std::shared_ptr<Services<Ts...>> impls;
+};
 } // namespace dic
--- CppDic/lib/include/cppdic/ServiceProviderBuilder.hpp
+++ CppDic/lib/include/cppdic/ServiceProviderBuilder.hpp
@@ -4,186 +4,177 @@
 #include <cppdic/ServiceProvider.hpp>
 #include <cppdic/Utils.hpp>
 
-namespace dic
-{
-    template<class C>
-    concept IsAbstract = std::is_abstract_v<C>;
+#include <memory>
+#include <tuple>
+#include <type_traits>
 
-    template<class C>
-    concept IsConcrete = !std::is_abstract_v<C>;
+namespace dic {
+template <class C>
+concept IsAbstract = std::is_abstract_v<C>;
 
-    template<class... Ts>
-    class [[nodiscard]] ServiceProviderBuilder final
-    {
-    public:
-        template<class T, class U, class V>
-        struct ConcatTupleTypes3;
+template <class C>
+concept IsConcrete = !std::is_abstract_v<C>;
 
-        template<class... Ts, class... Us, class... Vs>
-        struct ConcatTupleTypes3<
-            std::tuple<Ts...>,
-            std::tuple<Us...>,
-            std::tuple<Vs...>>
-        {
-            using CollectionWithConcatenatedTypes =
-                ServiceProviderBuilder<Ts..., Us..., Vs...>;
-        };
+template <class... Ts> class [[nodiscard]] ServiceProviderBuilder final {
+public:
+  template <class T, class U, class V> struct ConcatTupleTypes3;
 
-        [[nodiscard]] ServiceProviderBuilder() = default;
+  template <class... Ts2, class... Us, class... Vs>
+  struct ConcatTupleTypes3<std::tuple<Ts2...>, std::tuple<Us...>,
+                           std::tuple<Vs...>> {
+    using CollectionWithConcatenatedTypes =
+        ServiceProviderBuilder<Ts2..., Us..., Vs...>;
+  };
 
-    private:
-        ServiceProviderBuilder(Services<Ts...> impls) : impls(impls) {}
+  [[nodiscard]] ServiceProviderBuilder()
+      : impls(std::make_shared<Services<Ts...>>()) {}
 
-        friend class ServiceProviderBuilder;
+private:
+  ServiceProviderBuilder(std::shared_ptr<Services<Ts...>> impls)
+      : impls(std::move(impls)) {}
 
-    public:
-        template<IsConcrete Concrete>
-        auto addService()
-        {
-            return addServiceImpl<true, Concrete, Concrete>();
-        }
+  template <class...> friend class ServiceProviderBuilder;
 
-        template<
-            IsAbstract Iface,
-            IsConcrete Impl,
+public:
+  template <IsConcrete Concrete> auto addService() {
+    return addServiceImpl<true, Concrete, Concrete>();
+  }
+
+  template <IsAbstract Iface, IsConcrete Impl,
             class = std::enable_if_t<std::is_base_of_v<Iface, Impl>>>
-        auto addService()
-        {
-            return addServiceImpl<true, Iface, Impl>();
-        }
+  auto addService() {
+    return addServiceImpl<true, Iface, Impl>();
+  }
 
-        template<class T>
-        auto addService(std::shared_ptr<T> impl)
-        {
-            return addServiceImpl<false, T, T>(impl);
-        }
+  template <class T> auto addService(std::shared_ptr<T> impl) {
+    return addServiceImpl<false, T, T>(impl);
+  }
 
-        auto build()
-        {
-            auto finalImpls = impls;
-            initializeEmptyImpls<0, Ts...>(finalImpls);
-            return ServiceProvider<Ts...>(finalImpls);
-        }
+  auto build() {
+    auto finalImpls = impls;
+    auto provider = ServiceProvider<Ts...>(finalImpls);
+    auto providerRef = provider.ref();
+    initializeEmptyImpls<0, Ts...>(*finalImpls, providerRef);
+    return provider;
+  }
 
-    private:
-        template<bool PerformCreationChecks, class Iface, class Impl = Iface>
-        auto addServiceImpl(std::shared_ptr<Iface> impl = nullptr)
-        {
-            constexpr auto index = utils::getIndex<Iface, Ts...>(0);
+private:
+  template <bool PerformCreationChecks, class Iface, class Impl = Iface>
+  auto addServiceImpl(std::shared_ptr<Iface> impl = nullptr) {
+    constexpr auto index = utils::getIndex<Iface, Ts...>(0);
 
-            if constexpr (PerformCreationChecks)
-            {
-                using TypesBeforeThisOne = utils::
-                    TypesBeforeTupleAdapter<index, Services<Ts...>>::Types;
+    if constexpr (PerformCreationChecks) {
+      using TypesBeforeThisOne =
+          utils::TypesBeforeTupleAdapter<index, Services<Ts...>>::Types;
 
-                if constexpr (std::tuple_size_v<TypesBeforeThisOne> == 0)
-                {
-                    static_assert(
-                        std::is_default_constructible_v<Impl>,
-                        "Type has to be default-constructible, nothing else is "
-                        "in the collection yet.");
-                }
-                else
-                {
-                    static_assert(
-                        std::is_default_constructible_v<Impl>
-                            || utils::CanConstructTypeFromAListOfTuples<
-                                Impl,
-                                utils::Permutations<
-                                    TypesBeforeThisOne>::Types>::value,
-                        "Could not construct service only from services "
-                        "already "
-                        "registered in the builder.");
-                }
-            }
+      if constexpr (std::tuple_size_v<TypesBeforeThisOne> == 0) {
+        static_assert(
+            std::is_default_constructible_v<Impl> ||
+                std::constructible_from<Impl, ServiceProviderRef>,
+            "Type has to be default-constructible or accept "
+            "ServiceProviderRef, nothing else is in the collection "
+            "yet.");
+      } else {
+        static_assert(
+            std::is_default_constructible_v<Impl> ||
+                std::constructible_from<Impl, ServiceProviderRef> ||
+                utils::CanConstructTypeFromAListOfTuples<
+                    Impl, typename utils::Permutations<
+                              TypesBeforeThisOne>::Types>::value,
+            "Could not construct service only from services "
+            "already registered in the builder or from "
+            "ServiceProviderRef.");
+      }
+    }
 
-            if constexpr (sizeof...(Ts) == index)
-            {
-                return ServiceProviderBuilder<Ts..., Service<Iface, Impl>>(
-                    std::tuple_cat(
-                        impls, std::tuple<std::shared_ptr<Iface>>(impl)));
-            }
-            else
-            {
-                // Overwrite previously stored implementation (if any)
-                std::get<index>(impls) = impl;
+    if constexpr (sizeof...(Ts) == index) {
+      auto newImpls = std::make_shared<Services<Ts..., Service<Iface, Impl>>>(
+          std::tuple_cat(*impls, std::tuple<std::shared_ptr<Iface>>(impl)));
+      return ServiceProviderBuilder<Ts..., Service<Iface, Impl>>(newImpls);
+    } else {
+      // Overwrite previously stored implementation (if any)
+      std::get<index>(*impls) = impl;
 
-                return ConcatTupleTypes3<
-                    typename utils::TypesBefore<index, Ts...>::Types,
-                    std::tuple<Service<Iface, Impl>>,
-                    typename utils::TypesAfter<index, Ts...>::Types>::
-                    CollectionWithConcatenatedTypes(impls);
-            }
-        }
+      return ConcatTupleTypes3<
+          typename utils::TypesBefore<index, Ts...>::Types,
+          std::tuple<Service<Iface, Impl>>,
+          typename utils::TypesAfter<index, Ts...>::Types>::
+          CollectionWithConcatenatedTypes(impls);
+    }
+  }
 
-        template<size_t Index, class Head, class... Tail>
-        static void initializeEmptyImpls(auto& impls)
-        {
-            auto& impl = std::get<Index>(impls);
-            constexpr static bool IsNull =
-                !(std::is_same_v<Head::BaseType, Head::ImplType>
-                  && std::is_abstract_v<Head::BaseType>);
-            if constexpr (IsNull)
-            {
-                using Impl = Head::ImplType;
+  template <size_t Index, class Head, class... Tail>
+  static void initializeEmptyImpls(auto &impls,
+                                   const ServiceProviderRef &providerRef) {
+    auto &impl = std::get<Index>(impls);
+    constexpr static bool IsNull =
+        !(std::is_same_v<typename Head::BaseType, typename Head::ImplType> &&
+          std::is_abstract_v<typename Head::BaseType>);
+    if constexpr (IsNull) {
+      using Impl = typename Head::ImplType;
 
-                if constexpr (std::is_default_constructible_v<Impl>)
-                {
-                    impl = std::make_shared<Impl>();
-                }
-                else
-                {
-                    using Deps = utils::CanConstructTypeFromAListOfTuples<
-                        Impl,
-                        typename utils::Permutations<std::remove_cvref_t<
-                            decltype(impls)>>::Types>::Tuple;
-                    auto deps = SubsetCallWrapper<Deps>::selectSubsetPub(impls);
-                    static_assert(
-                        std::is_same_v<decltype(deps), Deps>,
+      constexpr bool CanWithProvider =
+          std::constructible_from<Impl, ServiceProviderRef>;
+
+      if constexpr (CanWithProvider) {
+        impl = std::make_shared<Impl>(providerRef);
+      } else if constexpr (std::is_default_constructible_v<Impl>) {
+        impl = std::make_shared<Impl>();
+      } else {
+        using TypesBeforeThisOne =
+            utils::TypesBeforeTupleAdapter<Index, Services<Ts...>>::Types;
+
+        if constexpr (std::tuple_size_v<TypesBeforeThisOne> == 0) {
+          // No services before this one and not default/provider constructible
+          // This will fail at runtime, but should have been caught at
+          // registration time
+          static_assert(false,
+                        "Service cannot be constructed with available "
+                        "dependencies");
+        } else {
+          using Deps = utils::CanConstructTypeFromAListOfTuples<
+              Impl,
+              typename utils::Permutations<TypesBeforeThisOne>::Types>::Tuple;
+          auto deps =
+              SubsetCallWrapper<Deps>::selectSubsetPub(impls);
+          static_assert(std::is_same_v<decltype(deps), Deps>,
                         "Sanity check failed");
-                    impl = utils::makeSharedFromTuple<Impl>(deps);
-                }
-            }
-
-            if constexpr (sizeof...(Tail) > 0)
-                initializeEmptyImpls<Index + 1, Tail...>(impls);
+          impl = utils::makeSharedFromTuple<Impl>(deps);
         }
+      }
+    }
 
-        template<class>
-        struct SubsetCallWrapper;
+    if constexpr (sizeof...(Tail) > 0)
+      initializeEmptyImpls<Index + 1, Tail...>(impls, providerRef);
+  }
 
-        template<class... Ts>
-        struct SubsetCallWrapper<std::tuple<Ts...>>
-        {
-            template<class Head, class... Tail>
-            static auto selectSubset(auto& impls, const auto& selectedSoFar)
-            {
-                if constexpr (sizeof...(Tail) == 0)
-                    return std::tuple_cat(
-                        selectedSoFar, std::tuple<Head>(std::get<Head>(impls)));
-                else
-                    return std::tuple_cat(
-                        selectedSoFar,
-                        selectSubset<Tail...>(impls, selectedSoFar));
-            }
+  template <class> struct SubsetCallWrapper;
 
-            template<class Head, class... Tail>
-            static auto selectSubset(auto& impls)
-            {
-                auto first = std::tuple<Head>(std::get<Head>(impls));
-                if constexpr (sizeof...(Tail) == 0)
-                    return first;
-                else
-                    return selectSubset<Tail...>(impls, first);
-            }
+  template <class... Ts3> struct SubsetCallWrapper<std::tuple<Ts3...>> {
+    template <class Head, class... Tail>
+    static auto selectSubset(auto &impls, const auto &selectedSoFar) {
+      if constexpr (sizeof...(Tail) == 0)
+        return std::tuple_cat(selectedSoFar,
+                              std::tuple<Head>(std::get<Head>(impls)));
+      else
+        return std::tuple_cat(selectedSoFar,
+                              selectSubset<Tail...>(impls, selectedSoFar));
+    }
 
-            static auto selectSubsetPub(auto& impls)
-            {
-                return selectSubset<Ts...>(impls);
-            }
-        };
+    template <class Head, class... Tail> static auto selectSubset(auto &impls) {
+      auto first = std::tuple<Head>(std::get<Head>(impls));
+      if constexpr (sizeof...(Tail) == 0)
+        return first;
+      else
+        return selectSubset<Tail...>(impls, first);
+    }
 
-    private:
-        Services<Ts...> impls;
-    };
+    static auto selectSubsetPub(auto &impls) {
+      return selectSubset<Ts3...>(impls);
+    }
+  };
+
+private:
+  std::shared_ptr<Services<Ts...>> impls;
+};
 } // namespace dic
