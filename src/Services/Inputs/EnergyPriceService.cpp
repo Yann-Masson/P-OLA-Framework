32a7cfdba3f09a3178a6d0c837cd0533412b543e
Antoine Grand <antoine.grand@epitech.eu>
2026-02-28 12:04:35 +0100

    feat(energyPrice): call the dataManageer for the energy price to get the data from the dataset

--- src/Services/Inputs/EnergyPriceService.cpp
+++ src/Services/Inputs/EnergyPriceService.cpp
@@ -7,10 +7,15 @@
 
 using namespace POLA::Common;
 using namespace POLA::Services::Inputs;
+using namespace POLA::Interfaces;
+using namespace POLA::Simulation;
 
 EnergyPriceData EnergyPriceService::getInput()
 {
-    return {
-        .pricePerKWh = 0.15
-    };
+    auto dataManager = _provider.get<Simulation::DataManager>();
+    auto clock = _provider.get<IClock>();
+    uint32_t currentTime = clock->getElapsedTimeSinceStart();
+    DataPoint dp = dataManager->getDataPointForTime(currentTime);
+    std::cout << "[EnergyPriceService] Providing energy price data for time " << dp.timestamp << "s: $" << dp.price_per_kWh << " per kWh" << std::endl;
+    return EnergyPriceData{dp.price_per_kWh};
 }
